0b1a1ad2723be803a2761ddbae4875e7b97df826
gness1804 <graham.nessler@gmail.com>
2016-08-09 15:27:18 -0600

    Initial commit--still need to figure out how to export data to Google heets

---
+++ script.js
@@ -0,0 +1,81 @@
+function assignAisle() {
+	var x = document.getElementById("category").value;
+	var y;
+	if (x === "Cooking Wines" || x === "Condiments" || x == "Olives") {
+		y = 2;
+	}
+	else if (x === "Baking Items" || x === "Spices") {
+		y = 5;
+	}
+	else if (x === "Canned Fish" || x === "Ethnic Foods" || x === "Pasta/Pasta Sauce" || x === "Rice") {
+		y = 6;
+	}
+	else if(x === "Canned Beans" || x === "Canned Tomatoes" || x === "Soups") {
+		y = 7;
+	}
+	else if (x === "Household Goods (Paper Towels, TP, etc)") {
+		y = 8;
+	}
+	else if (x === "Detergent (Dish and Laundry)") {
+		y = 9;
+	}
+	else if (x === "Breads/Bagels" || x === "Peanut Butter and Jelly") {
+		y = 13;
+	}
+	else if (x === "Frozen Lunches/Dinners") {
+		y = "11/12";
+	}
+	else if (x === "Frozen Veggies" || x === "Ice Cream") {
+		y = 12;
+	}
+	else if (x === "Honey") {
+		y = 13;
+	}
+	else if (x === "Office Supplies") {
+		y = 16;
+	}
+	else if (x === "Personal Care and Grooming") {
+		y = "17/18";
+	}
+	else if (x === "Medicines (OTC)") {
+		y = 18;
+	}
+	else if (x === "Chips" || x === "Nuts") {
+		y = 19;
+	}
+	else if (x === "Soda") {
+		y = 22;
+	}
+	else if (x === "Pest Control") {
+		y = "Front";
+	}
+	else if (x === "Meat") {
+		y = "Meat";
+	}
+	else if (x === "Produce") {
+		y = "Produce";
+	}
+	else if (x === "Seafood (Fresh or Frozen)") {
+		y = "Seafood";
+	}
+	document.getElementById("aisle").value = y;
+}
+
+function transfer () {
+	var para = document.createElement("TR");
+	var grocery = document.getElementById("grocery").value;
+	var t = document.createTextNode(grocery);
+	para.appendChild(t);
+	var p = document.getElementById("place");
+	p.appendChild(para);
+
+	var para2 = document.createElement("TR");
+	var aisle = document.getElementById("aisle").value;
+	var t2 = document.createTextNode("Aisle : " + aisle);
+	para2.appendChild(t2);
+	p.appendChild(para2);
+
+	document.getElementById("grocery").value = "";
+	document.getElementById("aisle").value = "";
+	document.getElementById("category").value = "Please choose a category.";
+};
